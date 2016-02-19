47b88c05505a0c8779c40acfa32bfa2c936ffc7f
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-18 16:22:35 -0800

    Implement 4:8 split for Trains page

--- build/App.js
+++ build/App.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -18,12 +18,13 @@ var App = function (_React$Component) {
   }
 
   _createClass(App, [{
-    key: 'render',
+    key: "render",
     value: function render() {
       return React.createElement(
-        'h1',
+        "div",
         null,
-        'React is so hot right now'
+        React.createElement("div", { className: "col-xs-4 leftThird" }),
+        React.createElement("div", { className: "col-xs-8 rightThird" })
       );
     }
   }]);
--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>React is so hot right now</h1>
+      <div>
+        <div className="col-xs-4 leftThird"></div>
+        <div className="col-xs-8 rightThird"></div>
+      </div>
     )
   }
 }
