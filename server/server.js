0db06255bbb2a1fa03de083a89b30329b87369bb
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-18 20:24:18 -0800

    Make successful GET request for current trains

--- server/server.js
+++ server/server.js
@@ -43,6 +43,7 @@ app.get('/api/trains', (req, res) => {
   // 12:30 PM - 1:30 PM
   const trains = [{
     trainId: 1,
+    stationId: 1,
     conductor: {id: 3, name: 'Griffin'},
     destinationName: 'Train Cafe',
     passengers: [{id: 1, name: 'Bobby'}, {id: 2, name: 'Batman'}],
@@ -53,6 +54,7 @@ app.get('/api/trains', (req, res) => {
   // 1:30 PM - 2:30 PM
   {
     trainId: 2,
+    stationId: 1,
     conductor: {id: 1, name: 'Bobby'},
     destinationName: 'Coffee Cafe',
     passengers: [{id: 3, name: 'Griffin'}, {id: 2, name: 'Batman'}],
