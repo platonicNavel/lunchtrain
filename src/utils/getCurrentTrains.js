d74b838348ee0e0ec6936e942d74d44b1940caa3
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-19 16:03:45 -0800

    Convert time from epoch to hh:mm

--- src/utils/getCurrentTrains.js
+++ src/utils/getCurrentTrains.js
@@ -5,6 +5,12 @@ const getCurrentTrains = (stationId, cb) => {
     type: 'GET',
     dataType: 'json',
     success: (data) => {
+      _.each(data, function(train) {
+        let td = new Date(train.timeDeparting*1000);
+        train.timeDeparting = `${td.getHours()}:${td.getMinutes()}`
+        let tb = new Date(train.timeBack*1000);
+        train.timeBack = `${tb.getHours()}:${tb.getMinutes()}`
+      });
       console.log(data)
       cb(data);
     },
