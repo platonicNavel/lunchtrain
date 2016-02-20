6bccc75ed517a187275ac2f69f98b09495e505d2
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-19 16:14:03 -0800

    Align train icon div and time div

--- src/TrainsListEntry.js
+++ src/TrainsListEntry.js
@@ -3,9 +3,11 @@ const TrainsListEntry = ({train, handleTrainBoardingClick}) =>
   return (
     <div className="trainEntry">
       <div className="trainContainer">
-        <div className="trainIcon"></div>
-        <div className="timeWrapper">
-          <div className="time">{train.timeDeparting}---{train.timeBack}</div>
+        <div className="trainIconTimeWrapper">
+          <div className="trainIcon"></div>
+          <div className="timeWrapper">
+            <div className="time">{train.timeDeparting}---{train.timeBack}</div>
+          </div>
         </div>
       </div>
     </div>
