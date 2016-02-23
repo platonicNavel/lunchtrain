1bdbd4b6e2ede22f641e5d4f40b6864aa1cc2920
Griffin Michl <griffinmichl@gmail.com>
2016-02-23 17:17:13 -0500

    add linter and grunt configs

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    eslint: {
+      src: [
+
+      ]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-eslint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+
+};
