69a137af7b4ffa265a7eed2cdeffffa3d22c7088
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-22 19:05:23 -0800

    Refactor TrainsListEntry to use class extends instead of arrow function
    If it makes sense, I will change it back to classless later

--- build/TrainsListEntry.js
+++ build/TrainsListEntry.js
@@ -1,81 +1,109 @@
 "use strict";
 
-var TrainsListEntry = function TrainsListEntry(_ref) {
-  var train = _ref.train;
-  var handleAccordionMap = _ref.handleAccordionMap;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  return React.createElement(
-    "div",
-    { className: "trainEntry", onClick: function onClick() {
-        return handleAccordionMap(train);
-      } },
-    React.createElement(
-      "div",
-      { className: "trainContainer" },
-      React.createElement(
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TrainsListEntry = function (_React$Component) {
+  _inherits(TrainsListEntry, _React$Component);
+
+  function TrainsListEntry(props) {
+    _classCallCheck(this, TrainsListEntry);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(TrainsListEntry).call(this, props));
+
+    _this.state = {
+      accordionClass: 'hidden',
+      open: false
+    };
+    return _this;
+  }
+
+  _createClass(TrainsListEntry, [{
+    key: "render",
+    value: function render() {
+      var train = this.props.train;
+      var handleAccordionMap = function handleAccordionMap(train) {
+        this.props.handleAccordionMap(train);
+      };
+      return React.createElement(
         "div",
-        { className: "trainIconTimeWrapper" },
-        React.createElement("div", { className: "trainIcon" }),
+        { className: "trainEntry", onClick: handleAccordionMap.bind(this, this.props.train) },
         React.createElement(
           "div",
-          { className: "timeWrapper" },
+          { className: "trainContainer" },
           React.createElement(
             "div",
-            { className: "time" },
-            train.timeDeparting,
-            "---",
-            train.timeBack
-          )
-        )
-      ),
-      React.createElement(
-        "div",
-        { className: "trainDetailsWrapper" },
-        React.createElement(
-          "h2",
-          { className: "trainDest" },
-          train.destinationName
-        ),
-        React.createElement(
-          "div",
-          { className: "trainRatingsWrapper" },
-          React.createElement(
-            "div",
-            { className: "likes col-xs-6" },
+            { className: "trainIconTimeWrapper" },
+            React.createElement("div", { className: "trainIcon" }),
             React.createElement(
               "div",
-              null,
-              "♥3"
+              { className: "timeWrapper" },
+              React.createElement(
+                "div",
+                { className: "time" },
+                train.timeDeparting,
+                "---",
+                train.timeBack
+              )
             )
           ),
           React.createElement(
             "div",
-            { className: "price col-xs-6" },
+            { className: "trainDetailsWrapper" },
+            React.createElement(
+              "h2",
+              { className: "trainDest" },
+              train.destinationName
+            ),
             React.createElement(
               "div",
-              null,
-              "$$"
+              { className: "trainRatingsWrapper" },
+              React.createElement(
+                "div",
+                { className: "likes col-xs-6" },
+                React.createElement(
+                  "div",
+                  null,
+                  "♥3"
+                )
+              ),
+              React.createElement(
+                "div",
+                { className: "price col-xs-6" },
+                React.createElement(
+                  "div",
+                  null,
+                  "$$"
+                )
+              )
             )
-          )
-        )
-      ),
-      React.createElement(
-        "div",
-        { className: "passengersWrapper" },
-        React.createElement(
-          "div",
-          { className: "conductor passenger" },
-          React.createElement("div", { className: "slackPic" })
-        ),
-        train.passengers.map(function (passenger) {
-          return React.createElement(
+          ),
+          React.createElement(
             "div",
-            { className: "passenger" },
-            React.createElement("div", { className: "slackPic" })
-          );
-        })
-      )
-    ),
-    React.createElement(TrainsListEntryDropdown, { train: train })
-  );
-};
+            { className: "passengersWrapper" },
+            React.createElement(
+              "div",
+              { className: "conductor passenger" },
+              React.createElement("div", { className: "slackPic" })
+            ),
+            train.passengers.map(function (passenger) {
+              return React.createElement(
+                "div",
+                { className: "passenger" },
+                React.createElement("div", { className: "slackPic" })
+              );
+            })
+          )
+        ),
+        React.createElement(TrainsListEntryDropdown, { train: train, className: this.state.accordionClass })
+      );
+    }
+  }]);
+
+  return TrainsListEntry;
+}(React.Component);
--- src/TrainsListEntry.js
+++ src/TrainsListEntry.js
@@ -1,33 +1,48 @@
-const TrainsListEntry = ({train, handleAccordionMap}) => 
-{
-  return (
-    <div className="trainEntry" onClick={() => handleAccordionMap(train)}>
-      <div className="trainContainer">
-        <div className="trainIconTimeWrapper">
-          <div className="trainIcon"></div>
-          <div className="timeWrapper">
-            <div className="time">{train.timeDeparting}---{train.timeBack}</div>
+class TrainsListEntry extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accordionClass: 'hidden',
+      open: false
+    };
+  }
+
+  render() {
+    let train = this.props.train;
+    let handleAccordionMap = function(train) {
+      this.props.handleAccordionMap(train);
+    }
+    return (
+      <div className="trainEntry" onClick={handleAccordionMap.bind(this, this.props.train)}>
+        <div className="trainContainer">
+          <div className="trainIconTimeWrapper">
+            <div className="trainIcon"></div>
+            <div className="timeWrapper">
+              <div className="time">{train.timeDeparting}---{train.timeBack}</div>
+            </div>
+          </div>
+          <div className="trainDetailsWrapper">
+            <h2 className="trainDest">{train.destinationName}</h2>
+            <div className="trainRatingsWrapper">
+              <div className="likes col-xs-6"><div>♥3</div></div>
+              <div className="price col-xs-6"><div>$$</div></div>
+            </div>
+          </div>
+          <div className="passengersWrapper">
+              <div className="conductor passenger">
+                <div className="slackPic"></div>
+              </div>
+              {train.passengers.map( passenger =>
+              <div className="passenger">
+                <div className="slackPic"></div>
+              </div>
+            )}
           </div>
         </div>
-        <div className="trainDetailsWrapper">
-          <h2 className="trainDest">{train.destinationName}</h2>
-          <div className="trainRatingsWrapper">
-            <div className="likes col-xs-6"><div>♥3</div></div>
-            <div className="price col-xs-6"><div>$$</div></div>
-          </div>
-        </div>
-        <div className="passengersWrapper">
-            <div className="conductor passenger">
-              <div className="slackPic"></div>
-            </div>
-            {train.passengers.map( passenger =>
-            <div className="passenger">
-              <div className="slackPic"></div>
-            </div>
-          )}
-        </div>
+        <TrainsListEntryDropdown train={train} className={this.state.accordionClass}></TrainsListEntryDropdown>
       </div>
-      <TrainsListEntryDropdown train={train}></TrainsListEntryDropdown>
-    </div>
-  )
+    )
+  }
 }
