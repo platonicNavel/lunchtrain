7b5a9fccdfbaba9b3c35ce0e9abe23f60b655926
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-22 16:48:50 -0800

    Init new react components for train dropdown

---
+++ build/TrainsListEntryDropdown.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var TrainsListEntryDropdown = function TrainsListEntryDropdown(_ref) {
+  var train = _ref.train;
+
+  return React.createElement(
+    "div",
+    { className: "trainEntryDropdown" },
+    React.createElement("div", { className: "trainEntryDropdownWrapper" })
+  );
+};
