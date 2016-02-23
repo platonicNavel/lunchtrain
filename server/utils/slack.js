6aea5b73cc2137412bb87079370e09de06b1c3b2
Griffin Michl <griffinmichl@gmail.com>
2016-02-23 17:24:02 -0500

    udpate db/index and utils/slack to conform to style guide

--- server/utils/slack.js
+++ server/utils/slack.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-//todo: alert the user that this channel has/will be created
+// todo: alert the user that this channel has/will be created
 function createChannel(token) {
   request({
     uri: `https://slack.com/api/channels.create?token=${token}&name=lunchtrain`,
@@ -10,24 +10,25 @@ function createChannel(token) {
   });
 }
 
-//can probably make these request more robust relative to the posting channel
+// can probably make these request more robust relative to the posting channel
 function trainAlert(conductor, destination, timeDeparting, token, scheduled) {
+  let str;
   if (scheduled) {
-    const str = `${conductor} has scheduled a train to ${destination} at ${timeDeparting}`;
+    str = `${conductor} has scheduled a train to ${destination} at ${timeDeparting}`;
   } else {
-    const str = `Train to ${destination} departing in 10 minutes`
+    str = `Train to ${destination} departing in 10 minutes`;
   }
   const queryStr = str.replace(' ', '%20');
   request({
     uri: `https://slack.com/api/chat.postMessage?token=${token}&channel=%23lunchtrain&text=${queryStr}&username=lunchtrain&icon_emoji=:bullettrain_side:`,
-    method: 'POST'
+    method: 'POST',
   }, (err, res, body) => {
-    //todo: record status code
-    console.log('Message posted');
+    // todo: record status code
+    console.log(body);
   });
 }
 
 module.exports = {
   createChannel,
   trainAlert,
-}
+};
