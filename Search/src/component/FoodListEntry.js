a288f1d9063e30564c5d0829f00a419321309130
Donghee Lee <jacobdhlee@gmail.com>
2016-02-23 18:25:19 -0800

    google place api work and show the multiple markers on the map

---
+++ Search/src/component/FoodListEntry.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FoodListEntry = () => {
+  return (
+    <li>adfss</li>
+  )
+}
+
+export default FoodListEntry
+
+
+// <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+//   {destinationName};
+// </button>
