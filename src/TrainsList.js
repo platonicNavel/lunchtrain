8dc5dc74b3968e3fb273fe3d4aa80ce9d4c6bc24
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-24 22:15:00 -0800

    Refactor trainListDropdown to be outside of the train entry

--- src/TrainsList.js
+++ src/TrainsList.js
@@ -1,11 +1,44 @@
-const TrainsList = ({trains, joinTrain}) => {
-  return (
-    <div className="trainsList">
-      {trains ?
-        trains.map( train =>
-          <TrainsListEntry key={train.trainId} train={train} joinTrain={joinTrain} />) :
-          "Looks like there are no trains here!"
-      }
-    </div>
-  )
+class TrainsList extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      accordionClass: 'details'
+    };
+  }
+
+  handleAccordionMap(e) {
+    console.log(this.state)
+    if(this.state.open) {
+      this.setState({
+        open: false,
+        accordionClass: "details"
+      });
+    }
+    else{
+      this.setState({
+        open: true,
+        accordionClass: "details open"
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="trainsList">
+        {this.props.trains ?
+          this.props.trains.map( train =>
+            <div class="trainAndDropdown" onClick={this.handleAccordionMap.bind(this)}>
+              <TrainsListEntry key={train.trainId} train={train} handleAccordionMap={this.props.handleAccordionMap} joinTrain={this.props.joinTrain} />
+              <TrainsListEntryDropdown train={train} open={this.state.open} accordionClass={this.state.accordionClass} />
+            </div>
+            ) :
+            "Looks like there are no trains here!"
+        }
+      </div>
+    )
+  }
+
 }
