d316d0df52638866f0901d4d61ffe5b574748225
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-24 19:41:10 -0800

    Refactor TrainsList to be a stateless component

--- src/TrainsList.js
+++ src/TrainsList.js
@@ -1,19 +1,11 @@
-class TrainsList extends React.Component {
-  
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="trainsList">
-        {this.props.trains ?
-          this.props.trains.map( train =>
-            <TrainsListEntry key={train.trainId} train={train} handleAccordionMap={this.props.handleAccordionMap} joinTrain={this.props.joinTrain} />) :
-            "Looks like there are no trains here!"
-        }
-      </div>
-    )
-  }
-
+const TrainsList = ({trains, joinTrain}) => {
+  return (
+    <div className="trainsList">
+      {trains ?
+        trains.map( train =>
+          <TrainsListEntry key={train.trainId} train={train} joinTrain={joinTrain} />) :
+          "Looks like there are no trains here!"
+      }
+    </div>
+  )
 }
