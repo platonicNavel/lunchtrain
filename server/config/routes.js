50909c0635b0460e738a214ac528c3ef5c1fe20e
Anthony Salierno <anthony.salierno@gmail.com>
2016-02-24 18:34:43 -0800

    Factor out actions from routes into utils.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,143 +1,30 @@
-const path = require('path');
-const _ = require('underscore');
 const auth = require('./auth');
-const db = require('../db/index');
+const utils = require('./utils.js');
 
 module.exports = function (app, express) {
 	
-  app.get('/', auth.ensureAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../../views/index.html'));
-  });
+  app.get('/', auth.ensureAuthenticated, utils.serveIndex);
 
-  app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../views/login.html'));
-  });
+  app.get('/login', utils.serveLogin);
 
-  app.get('/api/destinations', auth.ensureAuthenticated, (req, res) => {
-    const slackTeamId = req.user.slackTeamId;
-    db.Destination.findAll({
-      include: [{
-        model: db.Team,
-        where: { slackTeamId },
-      },
-      ],
-    }).then((destinations) => {
-      const formattedDestinations = destinations.map((destination) => {
-        const formattedDestination = {
-        name: destination.dataValues.name,
-        lat: destination.dataValues.lat,
-        long: destination.dataValues.long,
-        };
-        return formattedDestination;
-      });
-      res.send(formattedDestinations);
-    });
-  });
+  app.get('/api/destinations', auth.ensureAuthenticated, utils.getDestinations);
 
-  app.get('/api/trains', auth.ensureAuthenticated, (req, res) => {
-    // 
-    const slackTeamId = req.user.slackTeamId;
-    db.Train.findAll({
-      include: [
-      {
-        model: db.Team,
-        where: { slackTeamId },
-      },
-      {
-        model: db.User,
-      },
-      {
-        model: db.User,
-        as: 'Conductor',
-      },
-      {
-        model: db.Destination,
-      },
-      ],
-    }).then((trains) => {
-      const formattedTrains = trains.map((train) => {
-        const formattedTrain = {
-          id: train.dataValues.id,
-          timeDeparting: train.dataValues.timeDeparting,
-          timeDuration: train.dataValues.timeDuration,
-          // Use forEach for users, conductor and destination
-          users: train.dataValues.Users.map((user) => {
-            return _.pick(user, 'id', 'slackId', 'firstName', 'lastName');
-          }),
-          conductor: train.dataValues.Conductor,
-          destination: _.omit(train.dataValues.Destination.dataValues, 'createdAt', 'updatedAt'),
-        };
-        return formattedTrain;
-      });
-      res.send(formattedTrains);
-    });
-  });
+  app.get('/api/trains', auth.ensureAuthenticated, utils.getTrains);
 
-  app.get('/destinations', auth.ensureAuthenticated, (req, res) => {
-    res.render('destinations');
-  });
+  app.get('/destinations', auth.ensureAuthenticated, utils.serveDestinations);
 
-  app.get('/trains', auth.ensureAuthenticated, (req, res) => {
-    res.render('trains');
-  });
+  app.get('/trains', auth.ensureAuthenticated, utils.serveTrains);
 
-  app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/auth/slack');
-  });
+  app.get('/logout', utils.serveLogout);
 
   app.get('/auth/slack', auth.slackAuth);
 
-  app.get('/auth/slack/callback', auth.slackAuthCallback, (req, res) => {
-      // Successful authentication, redirect home.
-      res.redirect('/');
-    }
-  );
+  app.get('/auth/slack/callback', auth.slackAuthCallback, utils.indexRedirect);
 
-  app.post('/destinations', (req, res) => {
-    const user = req.user;
-    const data = req.body;
+  app.post('/destinations', utils.addDestination);
 
-    // todo: modularize, refactor
-    db.findOne({ where: { id: user.id } }).then((dbUser) => {
-      db.Train.create({
-        timeDeparting: data.timeDeparting,
-        timeDuration: data.timeDuration,
-      }).then((train) => {
-        train.addConductor(dbUser).then(() => {
-          db.Destination.findOrCreate({
-            googleId: data.googleId,
-            name: data.name,
-            lat: data.lat,
-            long: data.long,
-            visits: data.visits,
-            likes: data.likes,
-          }).spread(destination => destination.addTrain(train)).then(() => {
-            res.send(200, 'Train created');
-          });
-        });
-      });
-    });
-  });
+  app.post('/trains', utils.addTrain);
 
-  app.post('/trains', (req, res) => {
-    const data = req.body;
-    const user = req.user;
-    console.log(data, user.slackId);
-    db.User.findOne({
-      where: { slackId: user.slackId },
-    }).then((dbUser) => {
-      console.log(dbUser);
-      db.Train.findOne({
-        where: { id: data.id },
-      }).then(train => dbUser.addTrain(train)).then(() => {
-        res.send(200, 'Passenger added to train');
-      });
-    });
-  });
-
-  app.get('/*', (req, res) => {
-    res.send(404, "Page does not exist!");
-  });
+  app.get('/*', utils.serveNotFound);
 
 };
