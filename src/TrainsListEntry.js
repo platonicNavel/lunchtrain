c8c76303d12698a37fb7bc74162e50512b43fc68
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-27 21:06:20 -0800

    Prevent user from joining the same train twice

--- src/TrainsListEntry.js
+++ src/TrainsListEntry.js
@@ -1,35 +1,51 @@
-const TrainsListEntry = ({train, joinTrain}) => {
+class TrainsListEntry extends React.Component {
 
-  return (
-    <div className="trainEntry">
-      <div className="trainContainer">
-        <div className="trainIconTimeWrapper">
-          <div className="trainIcon"></div>
-          <div className="timeWrapper">
-            <div className="time">{train.timeDeparting} --- {train.timeBack}</div>
-          </div>
-        </div>
-        <div className="trainDetailsWrapper">
-          <h2 className="trainDest">{train.destination.name}</h2>
-          <div className="trainRatingsWrapper">
-            <div className="likes col-xs-6"><div>♥3</div></div>
-            <div className="price col-xs-6"><div>$$</div></div>
-          </div>
-        </div>
-        <div className="passengersWrapper">
-          <div className="conductor passenger">
-            <div className="slackPic">{train.conductor.firstName}</div>
-          </div>
-          {train.users.map( passenger =>
-            <div className="passenger">
-              <div className="slackPic">{passenger.firstName}</div>
+  constructor(props) {
+    super(props)
+    this.state = {
+      joined: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="trainEntry">
+        <div className="trainContainer">
+          <div className="trainIconTimeWrapper">
+            <div className="trainIcon"></div>
+            <div className="timeWrapper">
+              <div className="time">{this.props.train.timeDeparting} --- {this.props.train.timeBack}</div>
             </div>
-          )}
-          <div className="joinWrapper" onClick={(e) => joinTrain(e, train)}>
-            <div className="joinArrow">»</div>
+          </div>
+          <div className="trainDetailsWrapper">
+            <h2 className="trainDest">{this.props.train.destination.name}</h2>
+            <div className="trainRatingsWrapper">
+              <div className="likes col-xs-6"><div>♥3</div></div>
+              <div className="price col-xs-6"><div>$$</div></div>
+            </div>
+          </div>
+          <div className="passengersWrapper">
+            <div className="conductor passenger">
+              <div className="slackPic">{this.props.train.conductor.firstName}</div>
+            </div>
+            {this.props.train.users.map( passenger =>
+              <div className="passenger">
+                <div className="slackPic">{passenger.firstName}</div>
+              </div>
+            )}
+            <div className="joinWrapper" onClick={
+              (e) => {
+                this.props.joinTrain(e, this.props.train, this.state.joined);
+                this.setState({
+                  joined: true,
+                });
+              }
+            }>
+              <div className="joinArrow">»</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
