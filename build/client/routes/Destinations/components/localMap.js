7d72ae581d930b13d6fab8433bca017ae6067bbf
Griffin Michl <griffinmichl@gmail.com>
2016-02-28 17:57:51 -0800

    organize filesystem some mroe

---
+++ build/client/routes/Destinations/components/localMap.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var localMap = function localMap(props) {
+  var mapMarkers = function mapMarkers() {
+    props.onMapShow();
+  };
+  return _react2.default.createElement(
+    "div",
+    { id: "map" },
+    mapMarkers()
+  );
+};
+
+exports.default = localMap;
