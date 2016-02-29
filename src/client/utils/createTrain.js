c31e7b5e51fcf766f9600366427fc2a87ea51ccd
Griffin Michl <griffinmichl@gmail.com>
2016-02-29 15:48:16 -0800

    lint client utilities

--- src/client/utils/createTrain.js
+++ src/client/utils/createTrain.js
@@ -1,20 +1,19 @@
 import $ from 'jquery';
-import _ from 'lodash'
 
-const createTrain = (d, d2, place_id, name, lat, lng, visits, local) => {
-  console.log('LET\'S GOOOOOOOOOOOOOO', d, d2, place_id, name, lat, lng, visits)
+const createTrain = (d, d2, place_id, name, lat, lng, visits) => {
+  console.log('LET\'S GOOOOOOOOOOOOOO', d, d2, place_id, name, lat, lng, visits);
   $.ajax({
     url: '/destinations',
     type: 'POST',
     dataType: 'json',
     data: {
+      name,
+      lat,
+      visits,
       timeDeparting: d,
       timeDuration: d2,
       googleId: place_id,
-      name: name,
-      lat: lat,
       long: lng,
-      visits: visits,
     }
   });
 
