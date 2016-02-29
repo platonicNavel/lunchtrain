afb59b71df4ef9ab471bb6f9fc5d6e59251619c8
Anthony Salierno <anthony.salierno@gmail.com>
2016-02-29 12:04:45 -0800

    Handle prior merge conflict, routes not working

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -1,19 +1,11 @@
 const auth = require('./auth');
 const utils = require('./utils.js');
-// TO DELETE:
-const path = require('path');
 
 module.exports = (app) => {
   app.get('/', utils.serveIndex);
-  app.get('/landing', auth.ensureAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../views/landing.html'));
-  });
-  app.get('/login', utils.serveLogin);
   app.get('/api/loggedin', auth.checkAuthenticated);
-  app.get('/api/destinations', auth.ensureAuthenticated, utils.getDestinations);
-  app.get('/api/trains', auth.ensureAuthenticated, utils.getTrains);
-  // app.get('/destinations', auth.ensureAuthenticated, utils.serveDestinations);
-  // app.get('/trains', auth.ensureAuthenticated, utils.serveTrains);
+  app.get('/api/destinations', utils.getDestinations);
+  app.get('/api/trains', utils.getTrains);
   app.get('/logout', utils.serveLogout);
   app.get('/auth/slack', auth.slackAuth);
   app.get('/auth/slack/callback', auth.slackAuthCallback, utils.indexRedirect);
