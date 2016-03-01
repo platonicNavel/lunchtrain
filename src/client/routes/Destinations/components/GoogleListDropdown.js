25ecff6c540993f1a0d4bdb5c1bb39e796acf1d8
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-29 16:02:17 -0800

    Add input form into Destinations dropdown

--- src/client/routes/Destinations/components/GoogleListDropdown.js
+++ src/client/routes/Destinations/components/GoogleListDropdown.js
@@ -11,7 +11,16 @@ class GoogleListDropdown extends React.Component {
     this.state = {
       open: false,
       accordionClass: 'details',
+      departing: null,
+      returning: null,
     };
+  }
+
+  submitTrain(e, d, d2, place_id, name, lat, lng, visits) {
+    console.log(d, d2);
+    e.preventDefault();
+    e.stopPropagation();
+    createTrain(d, d2, place_id, name, lat, lng, visits);
   }
 
   render() {
@@ -21,19 +30,29 @@ class GoogleListDropdown extends React.Component {
         <div className="scheduleButtonWrapper">
           <div>
             <br />
-            <br />
             <p>Price: {this.props.item.price_level}</p>
             <p>Rating: {this.props.item.rating}</p>
             <p>Opening Now: {this.props.item.opening_hours.open_now}</p>
             <p>{this.props.item.vicinity}</p>
           </div>
-          <button className="scheduleButton" onClick={
+          <hr />
+            Schedule Train to {this.props.item.name}
+          <form onSubmit={
             (e) => {
-              this.props.createTrain(e, d, d2, this.props.item.place_id, this.props.item.name, this.props.item.geometry.location.lat(), this.props.item.geometry.location.lng(), 0);
+              e.preventDefault();
+              this.submitTrain(e, this.state.departing, this.state.returning, this.props.item.place_id, this.props.item.name, this.props.item.geometry.location.lat(), this.props.item.geometry.location.lng(), 0);
             }
           }>
-            schedule train {this.props.item.name}
-          </button>
+            <b>Departing</b>
+            <br />
+            <input type="datetime-local" value={this.state.departing} onClick={(e) => {e.preventDefault(); e.stopPropagation();}}/>
+            <br />
+            <b>Returning</b>
+            <br />
+            <input type="datetime-local" value={this.state.returning} onClick={(e) => {e.preventDefault(); e.stopPropagation();}}/>
+            <br />
+            <input type="submit" value="Submit" className="scheduleButton" />
+          </form>
         </div>
       </div>
     )
