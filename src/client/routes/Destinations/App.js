02fa3a41077deeeb84408ec31a86b119d12c8bf6
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-29 16:44:24 -0800

    Fix code after linter

--- src/client/routes/Destinations/App.js
+++ src/client/routes/Destinations/App.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
@@ -17,16 +16,16 @@ let service;
 let infowindow;
 
 class Destinations extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      recommend: false,
-      lat: null,
-      lng: null,
-      rendering: true,
-    };
-  }
+    constructor(props){
+      super(props);
+      this.state = {
+        list: [],
+        recommend: false,
+        lat: null,
+        lng: null,
+        rendering: true
+      } 
+    }
     // set the default HR lat lng, otherwise, users locationcheck
     // resultLocation is google place API default is restaurant and half mile radius
 
@@ -54,69 +53,98 @@ class Destinations extends Component {
           lng: +position.coords.longitude,
         })
       });
-    });
-  }
+    }
 
-  componentDidMount() {
-    this.navi();
-  }
-
-  onClicks() {
-    this.locatGetPlace();
-    this.setState({
-      recommend: true,
-    });
-  }
-
-  onRevese() {
-    this.getPlace();
-    this.setState({
-      recommend: false,
-      redering: true,
-    });
-  }
-
-  getPlace() {
-    const initMap = () => {
-      let city = { lat: this.state.lat, lng: this.state.lng };
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: city,
-        zoom: 15,
-      });
-
-      infowindow = new google.maps.InfoWindow();
-
-      service = new google.maps.places.PlacesService(map);
-      service.nearbySearch({
-        location: city,
-        radius: 500,
-        types: ['restaurant', 'cafe'],
-      }, callback);
+    componentDidMount() {
+      this.navi();
     };
 
-    const callback = (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-        this.setState({
-          list: results,
+    onClicks() {
+      this.locatGetPlace();
+      this.setState({
+        recommend: true
+      })
+    }
+
+    onRevese() {
+      this.getPlace();
+      this.setState({
+        recommend: false,
+        redering: true
+      })
+    }
+
+    getPlace() {
+      const initMap = () => {
+        let city = {lat: this.state.lat, lng: this.state.lng};
+          map = new google.maps.Map(document.getElementById("map"), {
+          center: city,
+          zoom: 15
         });
-        console.log('after ', this.state.list);
-      }
-    };
 
-    const createMarker = (place) => {
-      let placeLoc = place.geometry.location;
-      let marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location,
+        infowindow = new google.maps.InfoWindow();
+
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: city,
+          radius: 500,
+          types: ['restaurant','cafe']
+        }, callback);
+      };
+
+      const callback = (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+          this.setState({
+            list: results
+          });
+          console.log('after ', this.state.list)
+        }
+      };
+
+
+
+      const createMarker = (place) => {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      };
+      let iniRender;
+      if(this.state.redering === false) {
+        iniRender = '';
+      } else {
+        iniRender = initMap.bind(this);
+        this.setState({
+          redering: false
+        })
+      }
+      google.maps.event.addDomListener(window, 'load', setTimeout( iniRender, 3000) );
+    }
+    
+    locatGetPlace() {
+      $.ajax({
+        url: '/api/destinations',
+        type: 'GET',
+        datatype: 'json',
+        success: function(data) {
+          this.setState({
+            list: data
+          });
+          this.locations();
+          console.log('after local ', this.state.list);
+        },
+        error: function(data) {
+          console.error(data);
+        }
       });
-      google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-      });
-    };
 
       const locations = () => {
         var map = new google.maps.Map(document.getElementById('maps'), {
@@ -148,16 +176,14 @@ class Destinations extends Component {
     }
 
 
-  render() {
-    if (this.state.lat === null || this.state.lng === null) {
-      return (
-        <div>Loading...</div>
-      );
-    } else {
-      if (!this.state.recommend) {
+    render() {
+      if( this.state.lat === null || this.state.lng === null) {
         return (
-          <div>
-
+          <div>Loading...</div>
+        )
+      } else {
+        if( !this.state.recommend ) {
+          return(
             <div>
               <br />
                 <button onClick={this.onRevese.bind(this)}>Google</button>
@@ -173,17 +199,25 @@ class Destinations extends Component {
                 </div>
               </div>
             </div>
-            <div id="maps">
-              <localMap onMapShows={this.locatGetPlace.bind(this)}/>
-            </div>
+          )
+        } else {
+          return (
             <div>
-              <localLists list={this.state.list}/>
+              <div>
+                <button onClick={this.onRevese.bind(this)}>Google</button>
+                <button onClick={this.onClicks.bind(this)}>Recommendation</button>
+              </div>
+              <div id="maps">
+                <localMap onMapShows={this.locatGetPlace.bind(this)}/>
+              </div>
+              <div>
+                <localLists list={this.state.list}/>
+              </div>
             </div>
-          </div>
-        )
+          )
+        }
       }
     }
   }
-}
 export default Destinations;
 
