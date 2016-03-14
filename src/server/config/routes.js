2ec63f61ecefd8e10f7326eee90e503abbc4a010
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-13 21:08:43 -0700

    Redirect all non-api and non-auth GET requests to root path
    Ensures all clickable navigation is handled by react-router

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -3,6 +3,7 @@ const utils = require('./utils.js');
 
 module.exports = (app) => {
   app.get('/', utils.serveIndex);
+  app.get('/:var(trains|destinations|login)?', utils.serveIndex);
   app.get('/api/loggedin', auth.checkAuthenticated);
   app.get('/api/destinations', utils.getDestinations);
   app.get('/api/trains', utils.getTrains);
@@ -11,6 +12,5 @@ module.exports = (app) => {
   app.get('/auth/slack/callback', auth.slackAuthCallback, utils.indexRedirect);
   app.post('/destinations', utils.createTrain);
   app.post('/trains', utils.boardTrain);
-  app.get('/*', utils.serveNotFound);
 };
 
