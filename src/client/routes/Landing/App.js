89752d8688686d524c9de1f182090a192e81e7fe
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-05-13 18:36:51 -0700

    Style background for landing only

--- src/client/routes/Landing/App.js
+++ src/client/routes/Landing/App.js
@@ -15,24 +15,23 @@ class Landing extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row button-row">
-         <Link to="trains" onClick={this.componentWillUnmount}>
-            <div className=" btn btn-info">
-            <div className="dummy">Board a Train</div>
-              <img className="people" src="assets/people.png" />
-              <img className="train" src="assets/train.png" />
-              <div className="destinations-text">Join your friends on fun outings</div>
-            </div>
-          </Link>
-          <Link to="destinations" onClick={this.componentWillUnmount}>
-            <div className=" btn btn-info">
-              <div className="dummy">Schedule a Train</div>
-              <img className="calendar" src="assets/calendar.png" />
-              <img className="location" src="assets/location.png" />
-              <div className="trains-test">Organize trips to new places or old favorites</div>
-            </div>
-          </Link>
+      <div className="bg">
+        <div className="diag"></div>
+        <div className="container">
+          <div className="row button-row">
+           <Link to="trains" onClick={this.componentWillUnmount}>
+              <div className=" btn btn-info">
+              <div className="dummy">Board a Train</div>
+                <div className="destinations-text">Join your friends on fun outings</div>
+              </div>
+            </Link>
+            <Link to="destinations" onClick={this.componentWillUnmount}>
+              <div className=" btn btn-info">
+                <div className="dummy">Schedule a Train</div>
+                <div className="trains-test">Organize trips to new places or old favorites</div>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     )
